cb0c939f6bfe69f93cbfa6410636a584af35e39d
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2020-03-08 11:00:36 +0300

    add /logs

---
+++ README 2.md
@@ -0,0 +1,1 @@
+# lucky-7-team-bot
--- chat_bot_template.py
+++ chat_bot_template.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import json
+from datetime import datetime
 
 from setup import PROXY, TOKEN
 from telegram import Bot, Update
@@ -17,24 +19,68 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 
 
+def addlogging(function):
+    def wrapper(*args, **kwargs):
+        new_log = {
+            "user": args[0].effective_user.first_name,
+            "function": function.__name__,
+            "message": args[0].message.text,
+            "time": args[0].message.date.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+        }
+        id = datetime.today().timestamp()  # у каждого лога свой +- уникальный  id
+        log_to_update = {}  # без костыля не кошерно. log_to_update нужен для обновления logs.json
+        try:
+            with open("logs.json", "r") as f:
+                # по каким-то причинам не позволяет записывать в файл и одновременно читать из него
+                # пытаюсь сделать через "а" - перебрасывает в excepti. как убрать свистопляски с лишним костылем и избравиться от блока finally?
+                file_content = f.read()
+                log_to_update = json.loads(file_content)
+                log_to_update[id] = new_log
+        except:  # если logs.json пустой
+            log_to_update = {id: new_log}
+        finally:
+            with open("logs.json", "w") as write_file:
+                json.dump(log_to_update, write_file)
+        return function(*args, **kwargs)
+    return wrapper
+
+
+@addlogging
 def start(update: Update, context: CallbackContext):
     """Send a message when the command /start is issued."""
     update.message.reply_text(f'Привет, {update.effective_user.first_name}!')
 
 
+@addlogging
 def chat_help(update: Update, context: CallbackContext):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Введи команду /start для начала. ')
 
 
+@addlogging
 def echo(update: Update, context: CallbackContext):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
 
+@addlogging
 def error(update: Update, context: CallbackContext):
     """Log Errors caused by Updates."""
     logger.warning(f'Update {update} caused error {context.error}')
+
+
+def history(update: Update, context: CallbackContext):
+    """Send a message when the command /logs is issued."""
+    with open("logs.json", "r") as read_file:
+        data = list(json.load(read_file).values())
+        if len(data) > 5:
+            data = data[-1:-6:-1]
+        for log in data:
+            response = ""
+            for key, value in log.items():
+                response = response + f'{key}: {value}\n'
+            update.message.reply_text(response)
+
 
 
 def main():
@@ -47,6 +93,7 @@ def main():
     # on different commands - answer in Telegram
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', chat_help))
+    updater.dispatcher.add_handler(CommandHandler('history', history))
 
     # on noncommand i.e message - echo the message on Telegram
     updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
@@ -65,4 +112,5 @@ def main():
 
 if __name__ == '__main__':
     logger.info('Start Bot')
+    LOGS = []
     main()
